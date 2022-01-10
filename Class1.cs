6904b8168e67868421b0585909742b9ab3708337
Coffee <qntjd200@naver.com>
2022-01-10 14:25:14 +0900

    new class

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Class1
+    {
+        void NewClass()
+        {
+            System.Windows.Forms.MessageBox.Show("Test!");
+        }
+    }
+}
